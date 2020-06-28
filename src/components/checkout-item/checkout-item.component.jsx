7735c7e08918bb7a8f0b918f339bc5d80c5eb38f
rossanodan <rossanodan@gmail.com>
2020-06-28 21:02:37 +0100

    Removed usless selector and redux import

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,7 +1,4 @@
 import React, { useContext } from "react";
-import { connect } from "react-redux";
-
-import { clearItemFromCart } from "../../redux/cart/cart.actions";
 
 import { CartContext } from "../../providers/cart/cart.provider";
 
@@ -37,8 +34,4 @@ const CheckoutItem = ({ cartItem }) => {
   );
 };
 
-const mapDispatchToProps = (dispatch) => ({
-  clearItem: (item) => dispatch(clearItemFromCart(item)),
-});
-
-export default connect(null, mapDispatchToProps)(CheckoutItem);
+export default CheckoutItem;
